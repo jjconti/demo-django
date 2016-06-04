0c7a0f6ec46f098e29555a76a1d8df33953460c1
Juanjo Conti <jjconti@gmail.com>
2016-06-04 12:08:31 -0300

    Admin

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+import models
+
+admin.site.register(models.Localidad)
+admin.site.register(models.Cuenta)
+admin.site.register(models.Movimiento)
