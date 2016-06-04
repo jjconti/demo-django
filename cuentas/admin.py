f852ec3fcee3eac685b20d99316588449cf252a6
Juanjo Conti <jjconti@gmail.com>
2016-06-04 12:22:47 -0300

    date_hierarchy

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 import models
 
+
+class MovimientoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'fecha'
+
+
 admin.site.register(models.Localidad)
 admin.site.register(models.Cuenta)
-admin.site.register(models.Movimiento)
+admin.site.register(models.Movimiento, MovimientoAdmin)
