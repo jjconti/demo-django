9c279cd4c7b0bebffd367f0d82d869864d4fc03c
Juanjo Conti <jjconti@gmail.com>
2016-06-04 11:52:51 -0300

    Modelos

--- cuentas/models.py
+++ cuentas/models.py
@@ -3,3 +3,28 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Localidad(models.Model):
+
+    nombre = models.CharField(max_length=200)
+
+
+class Cuenta(models.Model):
+
+    nombre = models.CharField(max_length=200)
+    direccion = models.CharField(max_length=500)
+    localidad = models.ForeignKey(Localidad)
+    email = models.EmailField()
+
+    def __unicode__(self):
+        return "{0}-{1}-{2}".format(self.nombre, self.localidad, self.email)
+
+class Movimiento(models.Model):
+
+    cuenta = models.ForeignKey(Cuenta)
+    comprobante = models.TextField()
+    fecha = models.DateField()
+    signo = models.CharField(max_length=1, choices=['+', '-'])
+    importe = models.DecimalField(decimal_places=2)
+
+    def __unicode__(self):
+        return "{0}{1} ({3})".format(self.signo, self.importe, self.fecha)
