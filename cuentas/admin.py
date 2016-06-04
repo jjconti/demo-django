f0efa958a12ab80881a58ce9b718611c77365500
Juanjo Conti <jjconti@gmail.com>
2016-06-04 12:33:06 -0300

    list_filter

--- cuentas/admin.py
+++ cuentas/admin.py
@@ -3,10 +3,19 @@ from django.contrib import admin
 import models
 
 
+class LocalidadAdmin(admin.ModelAdmin):
+    pass
+
+
+class CuentaAdmin(admin.ModelAdmin):
+    list_filter = ('localidad',)
+
+
 class MovimientoAdmin(admin.ModelAdmin):
     date_hierarchy = 'fecha'
+    list_filter = ('cuenta__nombre',)
 
 
-admin.site.register(models.Localidad)
-admin.site.register(models.Cuenta)
+admin.site.register(models.Localidad, LocalidadAdmin)
+admin.site.register(models.Cuenta, CuentaAdmin)
 admin.site.register(models.Movimiento, MovimientoAdmin)
